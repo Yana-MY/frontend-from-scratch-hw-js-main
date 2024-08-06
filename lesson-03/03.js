b553f41fdeb65049a2a017cee3146364f2caca74
Yana <yano4ka87@mail.ru>
2024-08-06 21:44:52 +0300

    lesson3 hw3 done

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -2,8 +2,11 @@
 
 function findLargest(a,b,c) {
     if (a>b && a>c) {
-        
-    } else {
-        
+        return a   
+    } if (b>a && b>c) {
+        return b
+    }if (c>a && c>b) 
+    return c
     }
-}
+
+console.log(findLargest(10,5,4));
