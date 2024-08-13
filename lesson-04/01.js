f481accf1c95b4dcf250a19ac9f7b9f569e8de56
Yana <yano4ka87@mail.ru>
2024-08-13 17:55:45 +0300

    fix lesson4 hw1

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -17,7 +17,8 @@ function includesElement(array, element) {
     for (let i = 0; i < array.length; i++) {
         if (array[i] === element) {
             return true;
-        };
+        } else {
         return false;
+        }
     }
 }
