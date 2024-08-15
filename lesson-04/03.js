79e259bd43e7310bdec14c26a21a447a4fec85d0
Yana <yano4ka87@mail.ru>
2024-08-15 11:17:56 +0300

    lesson4 hw3 with 1function

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -18,28 +18,28 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 */
 // let array1 = [];
 // let array2 = [];
-function findCommonElements(array1, array2) {
-    const commonElements = [];
-    for (let i = 0; i < array1.length; i++) {
-        for (let j = 0; j < array2.length; j++) {
-            if (array1[i] === array2[j]) {
-                commonElements.push(array1[i]);
-            }
-        } 
-    }
-    return commonElements
-}
-console.log(findCommonElements([1, 2, 3], [2, 3, 4]))
-
-
 // function findCommonElements(array1, array2) {
-//     const newArray =[];
+//     const commonElements = [];
 //     for (let i = 0; i < array1.length; i++) {
-//         if (includesElement(array2, array1[i])) {
-//             newArray.push(array1[i]);
-//         }
-        
+//         for (let j = 0; j < array2.length; j++) {
+//             if (array1[i] === array2[j]) {
+//                 commonElements.push(array1[i]);
+//             }
+//         } 
 //     }
-//     return newArray
+//     return commonElements
 // }
-// console.log(findCommonElements([1, 2, 3], [2, 3, 4]));
+// console.log(findCommonElements([1, 2, 3, 4], [3, 6, 5]))
+
+
+function findCommonElements(array1, array2) {
+    const newArray =[];
+    for (let i = 0; i < array1.length; i++) {
+        if (includesElement(array2, array1[i])) {
+            newArray.push(array1[i]);
+        }
+        
+    }
+    return newArray
+}
+console.log(findCommonElements([1, 2, 3], [2, 3, 4]));
