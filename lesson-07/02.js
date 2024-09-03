5886f8e70189d212b231d2f6727e2e4ed2ac9e7e
Yana <yano4ka87@mail.ru>
2024-09-03 16:46:52 +0300

    fix lesson 7 hw 2 again

--- lesson-07/02.js
+++ lesson-07/02.js
@@ -17,10 +17,10 @@
 function isNumeric(str) {
   return !isNaN(str) && str !== ' ';
 }
-
+// isNumeric()
 // // Примеры использования
-// console.log(isNumeric("123"));     // Ожидаемый результат: true
-// console.log(isNumeric("12.3"));    // Ожидаемый результат: true
-// console.log(isNumeric("123abc"));   // Ожидаемый результат: false
-// console.log(isNumeric("abc"));      // Ожидаемый результат: false
-// console.log(isNumeric(" "));        // Ожидаемый результат: false
+console.log(isNumeric("123"));     // Ожидаемый результат: true
+console.log(isNumeric("12.3"));    // Ожидаемый результат: true
+console.log(isNumeric("123abc"));   // Ожидаемый результат: false
+console.log(isNumeric("abc"));      // Ожидаемый результат: false
+console.log(isNumeric(" "));        // Ожидаемый результат: false
